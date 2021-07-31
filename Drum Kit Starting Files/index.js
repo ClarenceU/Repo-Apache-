6ddc2c4f11c92bbd5d01af4255a0060c53141aa6
ClarenceU <84420687+ClarenceU@users.noreply.github.com>
2021-07-31 01:58:14 -0700

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,11 @@
+for (var i = 0; i<document.querySelectorAll(".drum").lenght)
+
+
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function{}) {
+  alert("I got clicked!");
+
+  //what to do when click detected.
+}};
+
+}
